4cda4e87ff3209907529527df7b5742623cfd158
YoonJoony <dbswns1101@naver.com>
2023-10-27 20:23:12 +0900

    setting

---
+++ src/main/java/com/datingacademia/datingacademia/DatingAcademiaApplication.java
@@ -0,0 +1,14 @@
+package com.datingacademia.datingacademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //스프링 시큐리티에서 자동으로 생성하는 로그인페이지 제거
+public class DatingAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datingacademia/datingacademia/DatingAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datingacademia.datingacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

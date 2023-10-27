b39068e77a4a8cf289b42bedd5d88ffca65f4213
YoonJoony <dbswns1101@naver.com>
2023-10-27 21:15:56 +0900

    setting test

--- src/main/java/com/datingacademia/datingacademia/DatingAcademiaApplication.java
+++ src/main/java/com/datingacademia/datingacademia/DatingAcademiaApplication.java
@@ -9,7 +9,7 @@ public class DatingAcademiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatingAcademiaApplication.class, args);
-		System.out.println("ADass");
+		System.out.println("ADassasd");
 	}
 
 }

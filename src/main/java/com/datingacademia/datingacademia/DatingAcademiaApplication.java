c5271121d92b1e9618ab08730542771c17b636a9
YoonJoony <dbswns1101@naver.com>
2023-10-27 21:25:19 +0900

    commit test

--- src/main/java/com/datingacademia/datingacademia/DatingAcademiaApplication.java
+++ src/main/java/com/datingacademia/datingacademia/DatingAcademiaApplication.java
@@ -9,7 +9,6 @@ public class DatingAcademiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatingAcademiaApplication.class, args);
-		System.out.println("ADasㅁㄴㅁㄴㅇㅇaㅁㄴㅇsdsasd");
 	}
 
 }
